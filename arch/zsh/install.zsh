496afb94d6b92aa1ebe5a8c7a7288d5f76d092d5
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-10-12 11:58:56 +0900

    update vim file

--- arch/zsh/install.zsh
+++ arch/zsh/install.zsh
@@ -2,7 +2,9 @@
 # 
 # base
 sudo pacman -S curl openssh tmux git
-sudo pacman -S go hugo php 
+sudo pacman -S go hugo php
+sudo pacman -S nodejs npm yarn
 sudo pacman -S docker docker-compose
 sudo yay -S mysql-clients
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
